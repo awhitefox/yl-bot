9294d13ab209ebe4cd6a6e974e53ca0958f0872c
awhitefox <ignat.k.150@gmail.com>
2021-04-23 17:18:17 +0300

    Add main.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import os
+from discord.ext import commands
+if os.path.isfile('../.env'):
+    from dotenv import load_dotenv
+    load_dotenv(encoding='utf8')
+
+bot = commands.Bot(os.environ['PREFIX'])
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+
+bot.run(os.environ['TOKEN'])
