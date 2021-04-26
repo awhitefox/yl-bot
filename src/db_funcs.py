4b8e4f32a6a068e21e3f5a992dd725bfa20a56a3
nikit0ska <nik.ryabov.2014@mail.ru>
2021-04-26 21:07:15 +0300

    db_funcs

--- src/db_funcs.py
+++ src/db_funcs.py
@@ -3,7 +3,7 @@ import psycopg2
 _conn = None
 
 
-def set_connection(uri: str, autocommit=False):
+def set_connection(uri: str, autocommit=False) -> psycopg2._psycopg.connection:
     global _conn
     if _conn is None:
         _conn = psycopg2.connect(uri)
@@ -11,17 +11,35 @@ def set_connection(uri: str, autocommit=False):
         return _conn
 
 
-def execute_query(query: str):
+def get_cursor() -> psycopg2._psycopg.cursor:
     global _conn
     if _conn is not None:
-        cursor = _conn.cursor()
-        cursor.execute(query)
-    return True
+        return _conn.cursor()
 
 
-def create_table(table_name, **kwargs):
+def execute_query(query: str) -> psycopg2._psycopg.cursor:
+    cursor = get_cursor()
+    cursor.execute(query)
+    return cursor
+
+
+def commit():
     global _conn
-    query = f"CREATE TABLE {table_name}("
-    for key in kwargs:
-        pass
+    if _conn is not None:
+        return _conn.commit()
 
+
+def db_read_table(table, limit=0, sql_condition='', order_by=('', '')) -> tuple:
+    cursor = get_cursor()
+    query = f'SELECT * FROM {table}'
+    if sql_condition != '':
+        query += f" where {sql_condition}"
+    if order_by != ('', ''):
+        query += f" order by {order_by[0]} {order_by[1]}"
+    if limit != 0:
+        query += f" limit {limit}"
+    query += ';'
+    cursor.execute(query)
+    data = cursor.fetchall()
+    cursor.cursor.commit()
+    return data
