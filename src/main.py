b5ca923554048f1eb651b03d1e1401f7248d784f
AWhiteFox <ignat.k.150@gmail.com>
2021-04-29 17:22:48 +0300

    Add custom help command

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import os
+import discord
 from discord.ext import commands
 if os.path.isfile('../.env'):
     from dotenv import load_dotenv
     load_dotenv(encoding='utf8')
 
-bot = commands.Bot(os.environ['PREFIX'])
+bot = commands.Bot(os.environ['PREFIX'], help_command=None)
 
 
 @bot.event
@@ -15,6 +16,12 @@ async def on_ready():
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(f':x: **Не удалось выполнить команду:** {error}')
+
+
+@bot.command(name='help')
+async def help_command(ctx):
+    embed = discord.Embed(title="Помощь по командам", description=os.environ['HELP'])
+    await ctx.send(embed=embed)
 
 
 bot.run(os.environ['TOKEN'])
