c2fd25b0e37919bf3645f4ea41cb1082827abdf0
AWhiteFox <ignat.k.150@gmail.com>
2021-04-29 23:41:30 +0300

    Set database connection in main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
 import discord
 from discord.ext import commands
+import db_funcs
+
 if os.path.isfile('../.env'):
     from dotenv import load_dotenv
     load_dotenv(encoding='utf8')
@@ -24,7 +26,8 @@ async def help_command(ctx: commands.Context):
     embed = discord.Embed(title='Помощь по командам', description=os.environ['HELP'])
     await ctx.send(embed=embed)
   
-  
+
+db_funcs.set_connection(os.environ['DATABASE_URL'])
 bot.load_extension('cogs.cats')
 bot.load_extension('cogs.weather')
 bot.run(os.environ['TOKEN'])
